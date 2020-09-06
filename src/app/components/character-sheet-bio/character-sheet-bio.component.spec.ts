b7537bba1053643afa44675a61d1b9ef37eaf3ed
FrancoisMontigny <francois.montigny@gmail.com>
2020-09-06 14:04:04 +0200

    feature-add-bootstrap fix: missing provider in unit test

--- src/app/components/character-sheet-bio/character-sheet-bio.component.spec.ts
+++ src/app/components/character-sheet-bio/character-sheet-bio.component.spec.ts
@@ -1,25 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CharacterSheetBioComponent } from './character-sheet-bio.component';
+import {CharacterSheetBioComponent} from './character-sheet-bio.component';
+import {FormBuilder} from '@angular/forms';
 
 describe('CharacterSheetBioComponent', () => {
-  let component: CharacterSheetBioComponent;
-  let fixture: ComponentFixture<CharacterSheetBioComponent>;
+    let component: CharacterSheetBioComponent;
+    let fixture: ComponentFixture<CharacterSheetBioComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CharacterSheetBioComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CharacterSheetBioComponent],
+            providers: [FormBuilder]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CharacterSheetBioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CharacterSheetBioComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
